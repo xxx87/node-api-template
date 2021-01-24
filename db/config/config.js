1f7db3f8c87b58b6e90c3083d0919148a2932905
ii87 <ii87@ukr.net>
2021-01-24 21:12:11 +0200

    sequelize-cli init

---
+++ db/config/config.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_DIALECT } = process.env;
+
+module.exports = {
+  development: {
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
+    host: DB_HOST,
+    dialect: DB_DIALECT,
+    logging: false
+  },
+  test: {
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
+    host: DB_HOST,
+    dialect: DB_DIALECT
+  },
+  production: {
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
+    host: DB_HOST,
+    dialect: DB_DIALECT
+  }
+};
